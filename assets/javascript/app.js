13dac3135bd9c231c44dec598bae9f86570a3659
Lei <leichen83@gmail.com>
2018-11-24 19:17:56 -0500

    almostfunctional

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,54 +1,41 @@
 $(document).ready(function() {
-  $("#resetb").hide();
-  $("#opt1").hide();
-  $("#opt2").hide();
-  $("#opt3").hide();
-  $("#opt4").hide();
-
-  var rightCount = 0;
-  var wrongCount = 0;
-  var unanswerCount = 0;
-  var intervalId;
-  var timer = 20;
-  var yourChose = "";
-  var running = false;
+  $("#resetb, #winner, #loser, #timeup").hide();
+  $("#opt1, #opt2, #opt3, #opt4").hide();
 
   var questions = [
     {
-      q1: "Which country hosted the 1966 World Cup?",
-      options: ["Brazil", "England", "Italy", "Uruguay"],
-      answer: 1
-    },
-    {
-      q2:
-        "Who scored the highest number of goals in World Cups 2002 and 2006 combined?",
-      options: [
-        "Alan Shearer",
-        "David Beckham",
-        "Michael Owen",
-        "Luis Ronaldo"
-      ],
-      answer: 3
-    },
-    {
-      q3: "When Brazil win the World Cup in 2002 it was their _____ triumph.",
-      options: ["7th", "8th", "5th", "6th"],
-      answer: 2
-    },
-    {
-      q4: "Which team has won the most World Cup games from 1930 to 2002?",
-      options: ["Brazil", "England", "Italy", "Uruguay"],
+      question: "Which country hosted the 1966 World Cup?",
+      option: ["Brazil", "England", "Italy", "Uruguay"],
       answer: 0
     },
     {
-      q5: "What country won the 1998 World Cup?",
-      options: ["Brazil", "England", "France", "Uruguay"],
+      question:
+        "Who scored the highest number of goals in World Cups 2002 and 2006 combined?",
+      option: ["Alan Shearer", "David Beckham", "Michael Owen", "Luis Ronaldo"],
+      answer: 3
+    },
+    {
+      question:
+        "When Brazil win the World Cup in 2002 it was their _____ triumph.",
+      option: ["7th", "8th", "5th", "6th"],
       answer: 2
     },
     {
-      q6:
+      question:
+        "Which team has won the most World Cup games from 1930 to 2002?",
+      option: ["Brazil", "England", "Italy", "Uruguay"],
+      answer: 0
+    },
+
+    {
+      question: "What country won the 1998 World Cup?",
+      option: ["Brazil", "England", "France", "Uruguay"],
+      answer: 2
+    },
+    {
+      question:
         "When the Bafana Bafana were chosen as the hosts of the 2010 World Cup they became the first African nation to be given that honor.",
-      options: ["Cameroon", "South Africa", "Egypt", "Brazil"],
+      option: ["Cameroon", "South Africa", "Egypt", "Brazil"],
       answer: 1
     }
   ];
@@ -57,19 +44,130 @@ $(document).ready(function() {
   var wrongCount = 0;
   var unanswerCount = 0;
   var intervalId;
-  var timer = 20;
-  var yourChose = "";
+  var timer = 10;
+  var yourChoice = "";
   var running = false;
+  var qCount = questions.length;
+  var pick;
+  var index;
+  var newArray = [];
+  var qholder = [];
 
   $("#startb").on("click", function() {
     $("#startb").hide();
     $("#wcup").hide();
-    showQ1();
+    showQuestions();
     runTimer();
+    for (var i = 0; i < questions.length; i++) {
+      holder.push(quetstions[i]);
+    }
   });
 
-  function showQ1() {
-    $("#qsection").html("<h3>Which country hosted the 1966 World Cup?</h3>");
-    
+  function runTimer() {
+    if (!running) {
+      intervalId = setInterval(decrement, 1000);
+      running = true;
+    }
   }
+
+  function decrement() {
+    $("#tremain").html("<h3>Time remaining: " + timer + "</h3>");
+    timer--;
+    if (timer === 0) {
+      unanswerCount++;
+      stop();
+      //$("#tremain").hide();
+      $("#qsection").html(
+        "<p>Your time is up! The correct answer is:" +
+          pick.option[pick.answer] +
+          "</p>"
+      );
+      $(".answerchoice").hide();
+      finish();
+    }
+  }
+
+  function stop() {
+    running = false;
+    clearInterval(intervalId);
+  }
+
+  function showQuestions() {
+    index = Math.floor(Math.random() * questions.length);
+    pick = questions[index];
+
+    $("#qsection").html(pick.question);
+    for (var i = 0; i < pick.option.length; i++) {
+      var userChoice = $("<div>");
+      userChoice.addClass("answerchoice");
+      userChoice.html(pick.option[i]);
+      userChoice.attr("data-guessvalue", i);
+      $("#asection").append(userChoice);
+    }
+
+    //////////////////////////////////
+
+    $(".answerchoice").on("click", function() {
+      //grab array position from userGuess
+      yourChoice = parseInt($(this).attr("data-guessvalue"));
+
+      //correct guess or wrong guess outcomes
+      if (yourChoice === pick.answer) {
+        stop();
+        rightCount++;
+        yourChoice = "";
+        $("#qsection").html("<p>Correct!</p>");
+        $(".answerchoice").hide();
+        finish();
+      } else {
+        stop();
+        wrongCount++;
+        yourChoice = "";
+        $("#qsection").html(
+          "<p>Wrong! The correct answer is: " +
+            pick.option[pick.answer] +
+            "</p>"
+        );
+        $(".answerchoice").hide();
+        finish();
+      }
+    });
+  }
+
+  function finish() {
+    newArray.push(pick);
+    questions.splice(index, 1);
+
+    var hidpic = setTimeout(function() {
+      $("#asection").empty();
+      timer = 10;
+
+      //run the score screen if all questions answered
+      if (wrongCount + rightCount + unanswerCount === qCount) {
+        $("#qsection").empty();
+        $("#qsection").html("<h3>Game Over!  Here's how you did: </h3>");
+        $("#asection").append("<h4> Correct: " + rightCount + "</h4>");
+        $("#asection").append("<h4> Incorrect: " + wrongCount + "</h4>");
+        $("#asection").append("<h4> Unanswered: " + unanswerCount + "</h4>");
+        $("#resetb").show();
+        rightCount = 0;
+        wrongCount = 0;
+        unanswerCount = 0;
+      } else {
+        runTimer();
+        showQuestions();
+      }
+    }, 3000);
+  }
+
+  $("#resetb").on("click", function() {
+    $("#resetb").hide();
+    $("#asection").empty();
+    $("#qsection").empty();
+    for (var i = 0; i < qholder.length; i++) {
+      questions.push(qholder[i]);
+    }
+    runTimer();
+    showQuestions();
+  });
 });
